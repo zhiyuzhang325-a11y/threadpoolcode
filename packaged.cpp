dbcca73590dceebd985df878060e5f7738d5931c
lYc <zhiyuzhang325@gmail.com>
2025-11-27 20:59:30 +0800

    测试提交

--- packaged.cpp
+++ packaged.cpp
@@ -13,11 +13,10 @@ public:
             works.emplace_back([this](){
                 while(true){
                     unique_lock<mutex>lock(mtx);
-                    cv.wait(lock, [this](){
+                    cv.wait(lock,[this](){
                         return stop || tasks.size();
                     });
                     if(stop && tasks.empty()) return;
-                    
                     auto task=move(tasks.front());
                     tasks.pop();
                     lock.unlock();
@@ -35,20 +34,36 @@ public:
         for(auto &t:works) t.join();
     }
     template<typename F,typename... Args>
-    void enqueue(F &&f, Args&&... args){
-        unique_lock<mutex>lock(mtx);
-        tasks.emplace(move(bind(forward<F>(f), forward<Args>(args)...)));
-        lock.unlock();
+    auto enqueue(F &&f,Args&&... args)->future<invoke_result_t<F,Args...>>{
+        using FType = invoke_result_t<F, Args...>;
+
+        auto task=make_shared<packaged_task<FType()>>(
+            bind(forward<F>(f),forward<Args>(args)...)
+        );
+
+        auto res=task->get_future();
+        {
+            unique_lock<mutex>lock(mtx);
+            tasks.emplace([task]{
+                (*task)();
+            });
+        }
         cv.notify_one();
+        return res;
     }
 };
 
 int main(){
     ThreadPool pool(4);
+    vector<future<int>>res;
     for(int i=0;i<10;i++){
-        pool.enqueue([i](){
+        res.emplace_back(pool.enqueue([i](){
             cout<<"task : "<<i<<" is running"<<endl;
+            this_thread::sleep_for(chrono::seconds(1));
             cout<<"task : "<<i<<" is finished"<<endl;
-        });
+            return i+10;
+        }));
+        cout<<"i = "<<i<<"result : "<<endl;
     }
+    for(auto &x:res) cout<<"results : "<<x.get()<<endl;
 }
